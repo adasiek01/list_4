fdd86805777f66e158c2e165dce9fa67a693b328
Adam Kawałko <262329@student.pwr.edu.pl>
2021-11-23 20:34:40 +0100

    3proba

--- L4_ZAD2.py
+++ L4_ZAD2.py
@@ -2,32 +2,46 @@
 Zaprojektuj i przeprowadź eksperyment porównujący wydajność implementacji kolejek QueueBaB i QueueBaE.
 """
 import time
+import matplotlib.pyplot as plt
 from L4_ZAD1 import QueueBaB, QueueBaE
 
 
-def comparing(a, b):
+def comparing(q, item):
     start_a = time.time()
-    for i in range(1, 1000):
-        a.enqueue("a")
-        a.enqueue("b")
-        a.enqueue("r")
-        a.dequeue()
-        a.is_empty()
-        a.size()
+    for i in range(1, 5000):
+        q.enqueue(item)
+    for i in range(1, 2500):
+        q.dequeue()
     end_a = time.time()
-    time_bab = end_a-start_a
     start_b = time.time()
-    for i in range(1, 1000):
-        b.enqueue("a")
-        b.enqueue("b")
-        b.enqueue("r")
-        b.dequeue()
-        b.is_empty()
-        b.size()
+    for i in range(1, 20000):
+        q.enqueue(item)
+    for i in range(1, 10000):
+        q.dequeue()
     end_b = time.time()
-    time_bae = end_b-start_b
-    return time_bab, time_bae
+    start_c = time.time()
+    for i in range(1, 10000):
+        q.enqueue(item)
+    for i in range(1, 5000):
+        q.dequeue()
+    end_c = time.time()
+    time_a = end_a - start_a
+    time_b = end_b - start_b
+    time_c = end_c - start_c
+    return time_a, time_b, time_c
 
+
+bab = comparing(QueueBaB(), 333)
+bae = comparing(QueueBaE(), 333)
 
 if __name__ == "__main__":
-    print(comparing(QueueBaB(), QueueBaE()))
+    print(bab)
+    print(bae)
+
+x = [5000, 10000, 20000]
+plt.plot(x, bab)
+plt.plot(x, bae)
+plt.title("comparison")
+plt.xlabel("steps")
+plt.ylabel("time")
+plt.show()
