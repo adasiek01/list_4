41888ec86e686576bd1df0a87981320388ce81fd
Adam Kawałko <262329@student.pwr.edu.pl>
2021-11-28 01:39:14 +0100

    pocz

--- L4_ZAD6.py
+++ L4_ZAD6.py
@@ -1,8 +1,156 @@
+class Node:
+
+    def __init__(self, init_data):
+        self.data = init_data
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self, new_data):
+        self.data = new_data
+
+    def set_next(self, new_next):
+        self.next = new_next
+
+
 class UnorderedList(object):
     """
     Tutaj skopiuj swoją implementację klasy UnorderedList,
     wykonaną jako rezultat Zadania 5.
     """
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.set_next(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.get_next()
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+
+        while not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+
+        if previous == None:  # jeśli usuwamy pierwszy element
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
+
+    def append(self, item):
+        """
+        Metoda dodająca element na koniec listy.
+        Przyjmuje jako argument obiekt, który ma zostać dodany.
+        Niczego nie zwraca.
+        """
+        current = self.head
+        last = False
+
+        while last == False:
+            if current.get_next() == None:
+                last = True
+            else:
+                current = current.get_next()
+        new_item = Node(item)
+        current.set_next(new_item)
+
+
+    def index(self, item):
+        """
+        Metoda podaje miejsce na liście,
+        na którym znajduje się określony element -
+        element pod self.head ma indeks 0.
+        Przyjmuje jako argument element,
+        którego pozycja ma zostać określona.
+        Zwraca pozycję elementu na liście lub None w przypadku,
+        gdy wskazanego elementu na liście nie ma.
+        """
+        current = self.head
+        ind = 0
+        while current.get_data() != item:
+            ind += 1
+            current = current.get_next()
+        else:
+            return ind
+
+
+    def insert(self, pos, item):
+        """
+        Metoda umieszcza na wskazanej pozycji zadany element.
+        Przyjmuje jako argumenty pozycję,
+        na której ma umiescić element oraz ten element.
+        Niczego nie zwraca.
+        Rzuca wyjątkiem IndexError w przypadku,
+        gdy nie jest możliwe umieszczenie elementu
+        na zadanej pozycji (np. na 5. miejsce w 3-elementowej liście).
+        """
+        current = self.head
+        for i in range(1, pos):
+            current = current.get_next()
+            #if current.get_next() == current:
+                #return IndexError
+        new_item = Node(item)
+        next = current.get_next()
+        current.set_next(new_item)
+        new_item.set_next(next)
+
+    def pop(self, pos=-1):
+        """
+        Metoda usuwa z listy element na zadaniej pozycji.
+        Przyjmuje jako opcjonalny argument pozycję,
+        z której ma zostać usunięty element.
+        Jeśli pozycja nie zostanie podana,
+        metoda usuwa (odłącza) ostatni element z listy.
+        Zwraca wartość usuniętego elementu.
+        Rzuca wyjątkiem IndexError w przypadku,
+        gdy usunięcie elementu z danej pozycji jest niemożliwe."""
+        current = self.head
+        for i in range(1, pos):
+            current = current.get_next()
+        to_remove = current.get_next()
+        next = to_remove.get_next()
+        current.set_next(next)
+
+
+    def __str__(self):
+        current = self.head
+        li = []
+        while current != None:
+            li.append(current.get_data())
+            current = current.get_next()
+        s = ("elements in the list are [" + ', '.join(['{}'] * len(li)) + "]")
+        return s.format(*li)
 
 
 class StackUsingUL(object):
@@ -15,6 +163,7 @@ class StackUsingUL(object):
         Nie pobiera argumentów.
         Zwraca True lub False.
         """
+        return self.items == None
 
     def push(self, item):
         """
@@ -22,6 +171,7 @@ class StackUsingUL(object):
         Pobiera element, który ma zostać umieszczony.
         Niczego nie zwraca.
         """
+
 
     def pop(self):
         """
