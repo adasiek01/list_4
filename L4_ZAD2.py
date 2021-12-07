241a66e00ce0840b0e11c1d4792cc8f2bd8747eb
Adam Kawałko <262329@student.pwr.edu.pl>
2021-12-07 21:54:36 +0100

    oki

--- L4_ZAD2.py
+++ L4_ZAD2.py
@@ -3,7 +3,98 @@ Zaprojektuj i przeprowadź eksperyment porównujący wydajność implementacji k
 """
 import time
 import matplotlib.pyplot as plt
-from L4_ZAD1 import QueueBaB, QueueBaE
+
+
+class QueueBaB(object):
+    """
+       Klasa implementująca kolejkę za pomocą pythonowej listy tak,
+       że początek kolejki jest przechowywany na początku listy.
+       """
+
+    def __init__(self):
+        self.list_of_items = []
+
+    def enqueue(self, item):
+        """
+        Metoda służąca do dodawania obiektu do kolejki.
+        Pobiera jako argument obiekt który ma być dodany.
+        Niczego nie zwraca.
+        """
+        self.list_of_items.append(item)
+
+    def dequeue(self):
+        """
+        Metoda służąca do ściągania obiektu do kolejki.
+        Nie pobiera argumentów.
+        Zwraca ściągnięty obiekt.
+        """
+        removed = self.list_of_items.pop(0)
+        return removed
+
+    def is_empty(self):
+        """
+        Metoda służąca do sprawdzania, czy kolejka jest pusta.
+        Nie pobiera argumentów.
+        Zwraca True jeśli kolejka jest pusta lub False gdy nie jest.
+        """
+        if len(self.list_of_items) == 0:
+            return True
+        else:
+            return False
+
+    def size(self):
+        """
+        Metoda służąca do określania wielkości kolejki.
+        Nie pobiera argumentów.
+        Zwraca liczbę obiektów w kolejce.
+        """
+        return len(self.list_of_items)
+
+
+class QueueBaE(object):
+    """
+    Klasa implementująca kolejkę za pomocą pythonowej listy tak,
+    że początek kolejki jest przechowywany na końcu listy.
+    """
+
+    def __init__(self):
+        self.list_of_items = []
+
+    def enqueue(self, item):
+        """
+        Metoda służąca do dodawania obiektu do kolejki.
+        Pobiera jako argument obiekt który ma być dodany.
+        Niczego nie zwraca.
+        """
+        self.list_of_items.insert(0, item)
+
+    def dequeue(self):
+        """
+        Metoda służąca do ściągania obiektu do kolejki.
+        Nie pobiera argumentów.
+        Zwraca ściągnięty obiekt.
+        """
+        removed = self.list_of_items.pop()
+        return removed
+
+    def is_empty(self):
+        """
+        Metoda służąca do sprawdzania, czy kolejka jest pusta.
+        Nie pobiera argumentów.
+        Zwraca True jeśli kolejka jest pusta lub False gdy nie jest.
+        """
+        if len(self.list_of_items) == 0:
+            return True
+        else:
+            return False
+
+    def size(self):
+        """
+        Metoda służąca do określania wielkości kolejki.
+        Nie pobiera argumentów.
+        Zwraca liczbę obiektów w kolejce.
+        """
+        return len(self.list_of_items)
 
 
 def comparing(n):
